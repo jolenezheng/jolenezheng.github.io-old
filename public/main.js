10608ea125775a3f5c67ef9d6a0cd66f9205ea32
Jacqueline Li <jacquelineli1005@gmail.com>
2019-09-14 19:18:21 -0400

    subtitles

--- public/main.js
+++ public/main.js
@@ -10,36 +10,39 @@ var delay = (function() {
       timer = setTimeout(callback, ms);
     };
   })();
+
+//   $(document).ready(function(){
+//   $('.youtubeUrl').on("click", function() {
+//     delay(function() {
+//       var videoUrl = $('#youtubeUrl').val();
+//       var videoID = videoUrl.substr(videoUrl.length - 11);
+//       var videos = "https://www.googleapis.com/youtube/v3/videos";
+//       var apiKey = "AIzaSyAzYHm1iwMocB9pW2uZrz_6Sqte5t_bXGo"; // Insert here your api key
+//       var fieldsTitle = "fields=items(snippet(title))";
+//       var fieldsEmpty = "";
+//       var part = "part=snippet";
   
-  $('#youtubeId').keyup(function() {
-    delay(function() {
-      var videoID = $('#youtubeId').val();
-      var videos = "https://www.googleapis.com/youtube/v3/videos";
-      var apiKey = "AIzaSyAzYHm1iwMocB9pW2uZrz_6Sqte5t_bXGo"; // Insert here your api key
-      var fieldsTitle = "fields=items(snippet(title))";
-      var fieldsEmpty = "";
-      var part = "part=snippet";
+//       function getUrl(fields) {
+//         var url = videos + "?" + "key=" + apiKey + "&" + "id=" + videoID + "&" + fields + "&" + part;
+//         return url;
+//       }
   
-      function getUrl(fields) {
-        var url = videos + "?" + "key=" + apiKey + "&" + "id=" + videoID + "&" + fields + "&" + part;
-        return url;
-      }
-  
-      $.get(getUrl(fieldsEmpty), function(response) {
-        var status = response.pageInfo.totalResults;
-        var title;
-        if (status) {
-          $.get(getUrl(fieldsTitle), function(response) {
-            title = response.items[0].snippet.title;
-            $('#info').text(title);
-            var url = "https://www.youtube.com/embed/" + videoID;
-            $('#myIframe').attr('src', url)
-          })
-        } else {
-          title = "Video doesn't exist";
-          $('#info').text(title);
-          $('#myIframe').attr('src', "");
-        }
-      });
-    }, 1000);
-  });
+//       $.get(getUrl(fieldsEmpty), function(response) {
+//         var status = response.pageInfo.totalResults;
+//         var title;
+//         if (status) {
+//           $.get(getUrl(fieldsTitle), function(response) {
+//             title = response.items[0].snippet.title;
+//             $('#info').text(title);
+//             var url = "https://www.youtube.com/embed/" + videoID;
+//             $('#myIframe').attr('src', url)
+//           })
+//         } else {
+//           title = "Video doesn't exist";
+//           $('#info').text(title);
+//           $('#myIframe').attr('src', "");
+//         }
+//       });
+//     }, 1000);
+//   });
+// });

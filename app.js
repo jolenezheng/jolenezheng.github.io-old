971275e48b4c2a271af4d183cab7712c56b23f45
jolenezheng <jolenezheng18@gmail.com>
2019-09-14 19:13:29 -0400

    updated app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// app.use('/js', express.static(path.join(__dirname + '/public/index.html')));
+app.use('/styles', express.static(path.join(__dirname + '/public/styles')));
+
 app.get("", function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 })

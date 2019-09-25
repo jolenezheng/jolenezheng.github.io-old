7d82f80eee1f0fd8180ded6d7252e2f767c8fab5
jolenezheng <jolenezheng18@gmail.com>
2019-09-24 20:40:40 -0400

    added particleJS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = init;
 
 // ensures page is fully loaded first
 function init() {
-    console.log("loaded")
-
+    console.log("loaded");
+    // particlesJS.load('particles-js', 'particles.json');
+    // particlesJS.load("particles-js", "particles-config.json")
 }

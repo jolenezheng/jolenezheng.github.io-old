29a49565edce8754908b0eb8cf6b5af87287ec03
jolenezheng <jolenezheng18@gmail.com>
2020-01-02 00:09:59 -0500

    added contact

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
+$(window).scroll(function() {
+  $(".content").css("opacity", 1 - $(window).scrollTop() / 250);
 });
